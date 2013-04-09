28ea4b362c481233344385507d6f9a3e488016e5
Chris Nord <msnorc@gmail.com>
2013-04-09 13:29:05 +0200

    starta jcombobox
    ikke fullført

--- src/streamfish/Reg_ordre.java
+++ src/streamfish/Reg_ordre.java
@@ -151,7 +151,7 @@ public class Reg_ordre extends javax.swing.JPanel {
 		int antPers = Integer.parseInt(jSpinner1.getValue().toString());
 
 
-		Order order = new Order(selMenu.getMenuId(), KUNDENR, 5/*TODO?*/, antPers, jTextField1.getText(), jTextField2.getText());
+		Order order = new Order(selMenu.getMenuId(), KUNDENR, 5/*TODO?*/, antPers, jTextField1.getText(), jComboBox1.(Kristian insert!);
 		gui.registerOrder(order);
 		gui.byttVindu(this, "streamfish.MainMenu");
 
