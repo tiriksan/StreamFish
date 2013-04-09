fcd39e989f42bf8fdb754f4ac90297943dcd0ef5
Sindre <sindre.aroen@yahoo.com>
2013-04-09 12:04:33 +0200

    Fjernet ArrayList og tilhørende metode i dish

--- src/streamfish/Dish.java
+++ src/streamfish/Dish.java
@@ -10,18 +10,15 @@ public class Dish {
         private String name;
         private String description;
         private String dishID;
-        private ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
         
-        public Dish(String name, String desc, ArrayList<Ingredient> ing){
+        public Dish(String name, String desc){
             this.name = name;
             this.description = desc;
-            this.ingredients = ing;
         }
         public Dish(String dishID, String name, String desc, ArrayList<Ingredient> ing){
             this.dishID = dishID;
             this.name = name;
             this.description = desc;
-            this.ingredients = ing;
         }
         public String getName(){
             return name;
@@ -34,25 +31,5 @@ public class Dish {
         }
         public void setDescription(String desc){
             description = desc;
-        }
-        public ArrayList<Ingredient> getIngredients(){
-            return ingredients;
-        }
-        public void addIngredient(Ingredient ing){
-            ingredients.add(ing);
-        }
-        public void removeIngredient(Ingredient ing){
-            ingredients.remove(ing);
-        }
-        public Ingredient findIngredient(String name){ //find ingridient by name
-            
-            for(Ingredient aIngredient : ingredients){
-                if(aIngredient.getName().equals(name)){
-                    return aIngredient;
-                }
-            }
-            
-            return null;
-        }
-        
+        }     
 }
