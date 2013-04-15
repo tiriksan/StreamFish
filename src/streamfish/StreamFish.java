bb5c37671a00a9b3cae5c9719b1d6ac48fe5a1d1
Charlie <HJ@trl185004.ka.hist.no>
2013-04-15 10:56:10 +0200

    equals < boolean-metode

--- src/streamfish/StreamFish.java
+++ src/streamfish/StreamFish.java
@@ -287,7 +287,7 @@ public class StreamFish {
 	public Employee userAuthorization(String username, String password) {
         for (Employee emp : getEmployees()) {
             if (username.equals(emp.getUsername())) {
-                if (password.equals(emp.getPassword())) {
+                if (emp.okPassword(password)) {
                     return emp;
                 }
             }
