56837e8f3e429c29d398531e19e8f7d6c8c04010
tiriksan <kristian.aabrekk@gmail.com>
2013-04-15 13:12:01 +0200

    byttvindu fix3

--- src/streamfish/Edit_customer.java
+++ src/streamfish/Edit_customer.java
@@ -201,17 +201,17 @@ public class Edit_customer extends javax.swing.JPanel {
 		Customer newCustomer = new Customer(KUNDENR, jTextField1.getText(), Integer.parseInt(jTextField2.getText()), jCheckBox1.isSelected());
 
 		gui.updateCustomer(newCustomer);
-		gui.byttVindu(this, "streamfish.MainMenu");
+		gui.byttVindu(this, new MainMenu(gui));
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
 		// TODO add your handling code here:
 		gui.changeCustomerStatus(customer);
-		gui.byttVindu(this, "streamfish.MainMenu");
+		gui.byttVindu(this, new MainMenu(gui));
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-		gui.byttVindu(this, "streamfish.MainMenu");
+		gui.byttVindu(this, new MainMenu(gui));
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
