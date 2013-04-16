7486852c1241f8ac6595223d911f7a990004e51f
Charlie <haakon.jarle.hassel@gmail.com>
2013-04-16 09:54:04 +0200

    Metode for å hente ut dagens dato på Databasevennlig form i Java

--- src/streamfish/TodaysDate.java
+++ src/streamfish/TodaysDate.java
@@ -1,11 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package streamfish;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.TimeZone;
+import java.util.Locale;
 
 /**
  *
@@ -17,10 +15,29 @@ public class TodaysDate {
     }
 
     public static String getDate() {
-        Calendar currentDate = Calendar.getInstance(); //Get the current date
+        TimeZone tz = TimeZone.getTimeZone("Europe/Oslo");
+        Calendar currentDate = Calendar.getInstance(tz, Locale.ENGLISH); //Get the current date
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MMM/dd"); //format it as per your requirement
         String dateNow = formatter.format(currentDate.getTime());
 
         return dateNow;
+    }
+    
+    public String getDateDB() {
+        TimeZone tz = TimeZone.getTimeZone("Europe/Oslo");
+        Calendar currentDate = Calendar.getInstance(tz, Locale.ENGLISH);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        String dateNow = formatter.format(currentDate.getTime());
+        
+        return dateNow;
+    }
+    
+    public static void main(String[] args) {
+//        String[] timezones = TimeZone.getAvailableIDs();
+//        for (String tz : timezones) {
+//            System.out.println(tz);
+//        }
+        TodaysDate td = new TodaysDate();
+        System.out.println(td.getDateDB());
     }
 }
