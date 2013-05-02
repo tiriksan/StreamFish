3ace6b771b5067d4425e27ef0bd53909df4bd94f
Chris Nord <msnorc@gmail.com>
2013-05-02 17:59:19 +0200

    GUI CLEANUP
    huehue

--- src/streamfish/GUI.java
+++ src/streamfish/GUI.java
@@ -14,357 +14,384 @@ import javax.swing.JPanel;
  */
 public class GUI extends JFrame implements WindowListener {
 
-	public static void main(String[] args) {
-		new GUI();
-	}
-	private ArrayList<JPanel> panels = new ArrayList<JPanel>();
-	private StreamFish db;
-	public int employee_id;
+    public static void main(String[] args) {
+        new GUI();
+    }
+    private ArrayList<JPanel> panels = new ArrayList<JPanel>();
+    private StreamFish db;
+    public int employee_id;
 
-	public GUI() {
-		db = new StreamFish();
-		addWindowListener(this);
+    public GUI() {
+        db = new StreamFish();
+        addWindowListener(this);
 
-		Login_screen login = new Login_screen(this);
-		//MainMenu mainMenu = new MainMenu(this);
+        Login_screen login = new Login_screen(this);
+        //MainMenu mainMenu = new MainMenu(this);
 
-		panels.add(login);
+        panels.add(login);
 //		panels.add(new MainMenu(this));
-		add(login);
-		login.setVisible(true);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setVisible(true);
-		pack();
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		double width = screenSize.getWidth();
-		double heigth = screenSize.getHeight();
-		Dimension frameSize = getSize();
-		setLocation((int) width / 2 - (int) frameSize.getWidth() / 2, (int) heigth / 2 - (int) frameSize.getHeight() / 2);
-	}
+        add(login);
+        login.setVisible(true);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+        pack();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double width = screenSize.getWidth();
+        double heigth = screenSize.getHeight();
+        Dimension frameSize = getSize();
+        setLocation((int) width / 2 - (int) frameSize.getWidth() / 2, (int) heigth / 2 - (int) frameSize.getHeight() / 2);
+    }
 
-	public void byttVindu(JPanel remove, JPanel add) {
-		remove(remove);
-		add(add);
-		pack();
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		double width = screenSize.getWidth();
-		double heigth = screenSize.getHeight();
-		Dimension frameSize = getSize();
-		setLocation((int) width / 2 - (int) frameSize.getWidth() / 2, (int) heigth / 2 - (int) frameSize.getHeight() / 2);
-	}
+    /**
+     * * ADD METHODS **
+     */
+    public boolean addCustomerAddress(CustomerAddress customer) {
+        int succ = db.addCustomerAddress(customer);
+        if (succ == 1) {
+            return true;
+        }
+        return false;
+    }
 
-	public boolean registerOrder(Order order) {
-		int suc = db.addOrder(order);
-		if (suc > 0) {
-			return true;
-		}
-		return false;
-	}
+    public boolean addDish(Dish dish) {
+        if (db.addDish(dish) == 1) {
+            return true;
+        }
+        return false;
+    }
 
-	public boolean registerCustomer(Customer customer) {
-		int suc = db.addCustomer(customer);
-		if (suc > 0) {
-			return true;
-		}
-		return false;
-	}
+    public boolean addDishIng(Integer dish_id, int ing_id, int amount) {
+        int succ = db.addDishIng(dish_id, ing_id, amount);
+        if (succ == 1) {
+            return true;
+        }
+        return false;
+    }
 
-	public boolean changeCustomerStatus(Customer customer) {
-		int succ = db.changeCustomerStatus(customer);
-		if (succ == 1) {
-			return true;
-		}
-		return false;
-	}
+    public boolean addIngredient(Ingredient ing) {
+        if (db.addIngredient(ing) == 1) {
+            return true;
+        }
+        return false;
+    }
 
-	public Customer[] getCustomers(String s, boolean status) {
-		return db.getCustomers(s, status);
-	}
+    /**
+     * * WINDOW HANDLING **
+     */
+    public void byttVindu(JPanel remove, JPanel add) {
+        remove(remove);
+        add(add);
+        pack();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double width = screenSize.getWidth();
+        double heigth = screenSize.getHeight();
+        Dimension frameSize = getSize();
+        setLocation((int) width / 2 - (int) frameSize.getWidth() / 2, (int) heigth / 2 - (int) frameSize.getHeight() / 2);
+    }
 
-	public Customer getCustomer(int i) {
-		return db.getCustomer(i);
-	}
+    /**
+     * * CAHNGE METHODS **
+     */
+    public boolean changeCustomerStatus(Customer customer) {
+        int succ = db.changeCustomerStatus(customer);
+        if (succ == 1) {
+            return true;
+        }
+        return false;
+    }
 
-	public boolean updateCustomer(Customer customer) {
-		int succ = db.updateCustomer(customer);
-		if (succ == 1) {
-			return true;
-		}
-		return false;
-	}
+    public int changeDishStatus(Dish dish) {
+        return db.changeDishStatus(dish);
+    }
 
-	public boolean addCustomerAddress(CustomerAddress customer) {
-		int succ = db.addCustomerAddress(customer);
-		if (succ == 1) {
-			return true;
-		}
-		return false;
-	}
+    public boolean changeEmployeeStatus(Employee emp) {
+        int suc = db.changeEmployeeStatus(emp);
+        if (suc == 1) {
+            return true;
+        }
+        return false;
+    }
 
-	public boolean registrerSubscription(Subscription subscription, Order order) {
-		if (db.registerSubscription(subscription, order) == 1) {
-			return true;
-		}
-		return false;
-	}
+    public boolean changeMenuStatus(Menu menu) {
+        int succ = db.changeMenuStatus(menu);
+        if (succ == 1) {
+            return true;
+        }
+        return false;
+    }
 
-	public void registerMenu(Menu menu, ArrayList<Integer> dishID) {
-		db.registerMenu(menu, dishID);
-	}
+    public int changeOrderFromSubStatus(int orderID, int subID) {
+        return db.changeOrderFromSubStatus(orderID, subID);
+    }
 
-	public void updateMenu(int menuId, Menu menu, ArrayList<Integer> dishID) {
-		db.updateMenu(menuId, menu, dishID);
-	}
+    public boolean changeOrderStatus(int orderID) {
+        int succ = db.changeOrderStatus(orderID);
+        if (succ == 1) {
+            return true;
+        }
+        return false;
+    }
 
-	public Orderinfo[] getTodaysTasks(String sok) {
-		return db.getTodaysTasks(sok);
-	}
+    public boolean changePassword(Employee emp) {
+        int suc = db.changePassword(emp);
+        if (suc == 1) {
+            return true;
+        }
+        return false;
+    }
 
-	public Orderinfo[] getTodaysTasks2(String sok) {
-		return db.getTodaysTasks2(sok);
-	}
+    /**
+     * * CHECK METHODS **
+     */
+    public String checkDateIng(int daysCheck) {
+        return db.checkDateIng(daysCheck);
+    }
 
-	public Order getOrderfromSub(Subscription subscription) {
-		return db.getOrder(subscription);
-	}
+    public String checkIngForDish(Dish dish) {
+        return db.checkIngForDish(dish);
+    }
 
-	public ArrayList<Order> getOrdersFormSub(Subscription subscription) {
-		return db.getOrderForSub(subscription);
-	}
+    /**
+     * * DELETE METHODS **
+     */
+    public int deleteSubscription(int id) {
+        return db.deleteSubscription(id);
+    }
 
-	public Order[] getOrders(Customer customer) {
-		return db.getOrder(customer);
-	}
+    /**
+     * *GET METHODS **
+     */
+    public CustomerAddress[] getAddress(int custid) {
+        return db.getAddress(custid);
+    }
 
-	public Subscription getSubscription(int subID) {
-		return db.getSubscription(subID);
-	}
+    public Customer getCustomer(int i) {
+        return db.getCustomer(i);
+    }
 
-	public Subscription[] getSubscriptions(String text) {
-		return db.getSubscriptions(text);
-	}
+    public Customer[] getCustomers(String s, boolean status) {
+        return db.getCustomers(s, status);
+    }
 
-	public boolean registerEmployee(Employee employee) {
-		int suc = db.addEmployee(employee);
-		if (suc > 0) {
-			return true;
-		}
-		return false;
-	}
+    public Dish getDish(int id) {
+        return db.getDish(id);
+    }
 
-	public boolean updateEmployee(Employee emp) {
-		int suc = db.updateEmployee(emp);
-		if (suc == 1) {
-			return true;
-		}
-		return false;
-	}
+    public int getDish() {
+        return db.getDish();
+    }
 
-	public boolean changePassword(Employee emp) {
-		int suc = db.changePassword(emp);
-		if (suc == 1) {
-			return true;
-		}
-		return false;
-	}
+    public ArrayList<Dish> getDishFromMenu(Menu menu) {
+        return db.getDishFromMenu(menu);
+    }
 
-	public boolean changeEmployeeStatus(Employee emp) {
-		int suc = db.changeEmployeeStatus(emp);
-		if (suc == 1) {
-			return true;
-		}
-		return false;
-	}
+    public int getDishIngAmount(int ingId, int dishId) {
+        return db.getDishIngAmount(ingId, dishId);
+    }
 
-	public Employee[] getEmployees(String s, boolean active) {
-		return db.getEmployees(s, active);
-	}
+    public Dish[] getDishes(String name) {
+        return db.getDishes(name);
+    }
 
-	public Employee getEmployee(int index) {
-		return db.getEmployee(index);
-	}
+    public Employee getEmployee(int index) {
+        return db.getEmployee(index);
+    }
 
-	public boolean changeOrderStatus(int orderID) {
-		int succ = db.changeOrderStatus(orderID);
-		if (succ == 1) {
-			return true;
-		}
-		return false;
-	}
+    public Employee[] getEmployees(String s, boolean active) {
+        return db.getEmployees(s, active);
+    }
 
-	public int changeOrderFromSubStatus(int orderID, int subID) {
-		return db.changeOrderFromSubStatus(orderID, subID);
-	}
+    public Ingredient[] getIngredients(String name) {
+        return db.getIngredients(name);
+    }
 
-	public CustomerAddress[] getAddress(int custid) {
-		return db.getAddress(custid);
-	}
+    public Menu getMenu(int menuID) {
+        return db.getMenu(menuID);
+    }
 
-	public Menu[] getMenus() {
-		return db.getMenus();
-	}
+    public ArrayList<String[]> getMenuSalesStats(String fromDate, String toDate, int ant, boolean top) {
+        return db.getMenuSalesStats(fromDate, toDate, ant, top);
+    }
 
-	public Menu[] getMenus(String sok, boolean inactive) {
-		return db.getMenus(sok, inactive);
-	}
+    public Menu[] getMenus(String sok, boolean inactive) {
+        return db.getMenus(sok, inactive);
+    }
 
-	public Menu getMenu(int menuID) {
-		return db.getMenu(menuID);
-	}
+    public Menu[] getMenus() {
+        return db.getMenus();
+    }
 
-	public ArrayList<Dish> getDishFromMenu(Menu menu) {
-		return db.getDishFromMenu(menu);
-	}
+    public ArrayList<String[]> getMonthlyRevenuePrEmployee(int month) {
+        return db.getMonthlyRevenuePrEmployee(month);
+    }
 
-	public boolean changeMenuStatus(Menu menu) {
-		int succ = db.changeMenuStatus(menu);
-		if (succ == 1) {
-			return true;
-		}
-		return false;
-	}
+    public ArrayList<String[]> getMostSpendingCustomers(int ant) {
+        return db.getMostSpendingCustomers(ant);
+    }
 
-	public int getDish() {
-		return db.getDish();
-	}
+    public Order getOrderfromSub(Subscription subscription) {
+        return db.getOrder(subscription);
+    }
 
-	public Dish getDish(int id) {
-		return db.getDish(id);
-	}
+    public Order[] getOrders(Customer customer) {
+        return db.getOrder(customer);
+    }
 
-	public boolean addDishIng(Integer dish_id, int ing_id, int amount) {
-		int succ = db.addDishIng(dish_id, ing_id, amount);
-		if (succ == 1) {
-			return true;
-		}
-		return false;
-	}
+    public ArrayList<Order> getOrdersFormSub(Subscription subscription) {
+        return db.getOrderForSub(subscription);
+    }
 
-	public int getDishIngAmount(int ingId, int dishId) {
-		return db.getDishIngAmount(ingId, dishId);
-	}
+    public Subscription getSubscription(int subID) {
+        return db.getSubscription(subID);
+    }
 
-	public int changeDishStatus(Dish dish) {
-		return db.changeDishStatus(dish);
-	}
+    public Subscription[] getSubscriptions(String text) {
+        return db.getSubscriptions(text);
+    }
 
-	public Employee getUserAuthorization(String username, String password) {
-		return db.userAuthorization(username, password);
-	}
+    public Orderinfo[] getTodaysTasks(String sok) {
+        return db.getTodaysTasks(sok);
+    }
 
-	public Dish[] getDishes(String name) {
-		return db.getDishes(name);
-	}
+    public Orderinfo[] getTodaysTasks2(String sok) {
+        return db.getTodaysTasks2(sok);
+    }
 
-	public String checkIngForDish(Dish dish) {
-		return db.checkIngForDish(dish);
-	}
+    public ArrayList<String[]> getTotalRevenuePrEmployee() {
+        return db.getTotalRevenuePrEmployee();
+    }
 
-	public String checkDateIng(int daysCheck) {
-		return db.checkDateIng(daysCheck);
-	}
+    public Employee getUserAuthorization(String username, String password) {
+        return db.userAuthorization(username, password);
+    }
 
-	public boolean addDish(Dish dish) {
-		if (db.addDish(dish) == 1) {
-			return true;
-		}
-		return false;
-	}
+    public String getYearlyRevenue(int year) {
+        return db.getYearlyRevenue(year);
+    }
 
-	public int deleteSubscription(int id) {
-		return db.deleteSubscription(id);
-	}
-	//public int changeOrderSubStatus(int orderID, int subID){
-	// return db.changeOrderSubStatus(orderID, subID);
-	//}
+    /**
+     * * REGISTER **
+     */
+    public boolean registerCustomer(Customer customer) {
+        int suc = db.addCustomer(customer);
+        if (suc > 0) {
+            return true;
+        }
+        return false;
+    }
 
-	public Ingredient[] getIngredients(String name) {
-		return db.getIngredients(name);
-	}
+    public boolean registerEmployee(Employee employee) {
+        int suc = db.addEmployee(employee);
+        if (suc > 0) {
+            return true;
+        }
+        return false;
+    }
 
-	public boolean addIngredient(Ingredient ing) {
-		if (db.addIngredient(ing) == 1) {
-			return true;
-		}
-		return false;
-	}
+    public void registerMenu(Menu menu, ArrayList<Integer> dishID) {
+        db.registerMenu(menu, dishID);
+    }
 
-	public boolean updateIngredient(Ingredient ingToUpdate, Ingredient ing) {
-		int succ = db.updateIngredient(ingToUpdate, ing);
-		if (succ == 1) {
-			MainMenu main = (MainMenu) panels.get(1);
-			main.updt();
-			return true;
-		}
-		return false;
-	}
+    public boolean registerOrder(Order order) {
+        int suc = db.addOrder(order);
+        if (suc > 0) {
+            return true;
+        }
+        return false;
+    }
 
-	public boolean updateDish(Dish dishToUpdate, Dish dish) {
-		int succ = db.updateDish(dishToUpdate, dish);
-		if (succ == 1) {
-			return true;
-		}
-		return false;
-	}
+    public boolean registrerSubscription(Subscription subscription, Order order) {
+        if (db.registerSubscription(subscription, order) == 1) {
+            return true;
+        }
+        return false;
+    }
 
-	public int updateDishIng(Ingredient ing, Dish dish, int amount) {
-		return db.updateDishIng(ing, dish, amount);
-	}
+    /**
+     * * SET METHODS **
+     */
+    public boolean setDeliveredOrders() {
+        int succ = db.setDeliveredOrders();
+        if (succ != -1) {
+            return true;
+        }
+        return false;
+    }
 
-	public boolean setDeliveredOrders() {
-		int succ = db.setDeliveredOrders();
-		if (succ != -1) {
-			return true;
-		}
-		return false;
-	}
+    /**
+     * * UPDATE METHODS **
+     */
+    public boolean updateCustomer(Customer customer) {
+        int succ = db.updateCustomer(customer);
+        if (succ == 1) {
+            return true;
+        }
+        return false;
+    }
 
-	public ArrayList<String[]> getMenuSalesStats(String fromDate, String toDate, int ant, boolean top) {
-		return db.getMenuSalesStats(fromDate, toDate, ant, top);
-	}
+    public boolean updateDish(Dish dishToUpdate, Dish dish) {
+        int succ = db.updateDish(dishToUpdate, dish);
+        if (succ == 1) {
+            return true;
+        }
+        return false;
+    }
 
-	public ArrayList<String[]> getMonthlyRevenuePrEmployee(int month) {
-		return db.getMonthlyRevenuePrEmployee(month);
-	}
+    public int updateDishIng(Ingredient ing, Dish dish, int amount) {
+        return db.updateDishIng(ing, dish, amount);
+    }
 
-	public ArrayList<String[]> getTotalRevenuePrEmployee() {
-		return db.getTotalRevenuePrEmployee();
-	}
+    public boolean updateEmployee(Employee emp) {
+        int suc = db.updateEmployee(emp);
+        if (suc == 1) {
+            return true;
+        }
+        return false;
+    }
 
-	public ArrayList<String[]> getMostSpendingCustomers(int ant) {
-		return db.getMostSpendingCustomers(ant);
-	}
+    public boolean updateIngredient(Ingredient ingToUpdate, Ingredient ing) {
+        int succ = db.updateIngredient(ingToUpdate, ing);
+        if (succ == 1) {
+            MainMenu main = (MainMenu) panels.get(1);
+            main.updt();
+            return true;
+        }
+        return false;
+    }
 
-	public String getYearlyRevenue(int year) {
-		return db.getYearlyRevenue(year);
-	}
+    public void updateMenu(int menuId, Menu menu, ArrayList<Integer> dishID) {
+        db.updateMenu(menuId, menu, dishID);
+    }
 
-	@Override
-	public void windowOpened(WindowEvent e) {
-	}
+    /**
+     * * WINDOW ACTIVITY **
+     */
+    @Override
+    public void windowOpened(WindowEvent e) {
+    }
 
-	@Override
-	public void windowClosing(WindowEvent e) {
-		db.close();
-	}
+    @Override
+    public void windowClosing(WindowEvent e) {
+        db.close();
+    }
 
-	@Override
-	public void windowClosed(WindowEvent e) {
-	}
+    @Override
+    public void windowClosed(WindowEvent e) {
+    }
 
-	@Override
-	public void windowIconified(WindowEvent e) {
-	}
+    @Override
+    public void windowIconified(WindowEvent e) {
+    }
 
-	@Override
-	public void windowDeiconified(WindowEvent e) {
-	}
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+    }
 
-	@Override
-	public void windowActivated(WindowEvent e) {
-	}
+    @Override
+    public void windowActivated(WindowEvent e) {
+    }
 
-	@Override
-	public void windowDeactivated(WindowEvent e) {
-	}
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+    }
 }
